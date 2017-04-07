b78d096d1afcf38c5a36b45e93d997d97dd8ce05
Suzanne <suzanne.aitchison@gmail.com>
2017-04-07 10:43:50 +0100

    record store has city

--- specs/record_store_spec.js
+++ specs/record_store_spec.js
@@ -3,9 +3,13 @@ var RecordStore = require('../record_store.js')
 
 describe('RecordStore tests', function(){
 
-  var recordStore = new RecordStore('CC Records')
+  var recordStore = new RecordStore('CC Records', 'Edinburgh')
 
   it('has a name', function(){
     assert.strictEqual('CC Records', recordStore.name)
+  })
+
+  it('has a city', function(){
+    assert.strictEqual('Edinburgh', recordStore.city)
   })
 })
