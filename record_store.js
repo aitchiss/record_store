22287f992c18ab5954bb5377a441e772dfb77cff
Suzanne <suzanne.aitchison@gmail.com>
2017-04-07 10:48:01 +0100

    can enter initial balance for record store

--- record_store.js
+++ record_store.js
@@ -1,7 +1,9 @@
-var RecordStore = function(name, city){
+var RecordStore = function(name, city, initialBalance){
   this.name = name
   this.city = city
+  this.balance = initialBalance
   this.inventory = []
+
 }
 
 
--- specs/record_store_spec.js
+++ specs/record_store_spec.js
@@ -3,7 +3,7 @@ var RecordStore = require('../record_store.js')
 
 describe('RecordStore tests', function(){
 
-  var recordStore = new RecordStore('CC Records', 'Edinburgh')
+  var recordStore = new RecordStore('CC Records', 'Edinburgh', 10000)
 
   it('has a name', function(){
     assert.strictEqual('CC Records', recordStore.name)
@@ -15,5 +15,9 @@ describe('RecordStore tests', function(){
 
   it('has an empty inventory when initialized', function(){
     assert.strictEqual(0, recordStore.inventory.length)
+  })
+
+  it('has a 10000 balance', function(){
+    assert.strictEqual(10000, recordStore.balance)
   })
 })
