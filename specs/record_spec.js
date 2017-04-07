2c9330668021d9351c9ce23fc64cbf58aef839da
Suzanne <suzanne.aitchison@gmail.com>
2017-04-07 10:36:00 +0100

    record has title

--- specs/record_spec.js
+++ specs/record_spec.js
@@ -3,9 +3,13 @@ var Record = require('../record.js')
 
 describe('Record tests', function(){
 
-  var record = new Record("David Bowie")
+  var record = new Record('David Bowie', 'Hunky Dory')
 
   it('has an artist', function(){
     assert.strictEqual('David Bowie', record.artist)
+  })
+
+  it('has a title', function(){
+    assert.strictEqual('Hunky Dory', record.title)
   })
 })
