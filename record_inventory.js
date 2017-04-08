c910e026ecc0bcfc0bf9f28f26c01d60a303bc8d
Suzanne <suzanne.aitchison@gmail.com>
2017-04-08 11:29:58 +0100

    can add to inventory

--- record_inventory.js
+++ record_inventory.js
@@ -2,4 +2,10 @@ var RecordInventory = function(){
   this.inventory = []
 }
 
+RecordInventory.prototype = {
+  add: function(record){
+    this.inventory.push(record)
+  }
+}
+
 module.exports = RecordInventory
--- specs/record_inventory_spec.js
+++ specs/record_inventory_spec.js
@@ -1,11 +1,22 @@
 var assert = require('assert')
 var RecordInventory = require('../record_inventory.js')
+var Record = require('../record.js')
 
 describe('Record Inventory tests', function(){
 
-  var recordInventory = new RecordInventory
+  var recordInventory 
+
+  beforeEach(function(){
+    recordInventory = new RecordInventory
+  })
 
   it('starts with empty inventory', function(){
     assert.strictEqual(0, recordInventory.inventory.length)
+  })
+
+  it('can add to inventory', function(){
+    var record1 = new Record('Bruce Springsteen', 'Darkness on the Edge of Town', 'rock', 1299)
+    recordInventory.add(record1)
+    assert.strictEqual(1, recordInventory.inventory.length)
   })
 })
