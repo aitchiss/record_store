79458771dd891c3ff27483e4387ab7fd5a7c82e9
Suzanne <suzanne.aitchison@gmail.com>
2017-04-08 11:14:08 +0100

    buying a record descreases funds

--- record_collector.js
+++ record_collector.js
@@ -2,5 +2,11 @@ var RecordCollector = function(initialFunds){
   this.cash = initialFunds
 }
 
+RecordCollector.prototype = {
+  buy: function(record){
+    this.cash -= record.price
+  }
+}
+
 
 module.exports = RecordCollector
--- specs/record_collector_spec.js
+++ specs/record_collector_spec.js
@@ -1,12 +1,23 @@
 var assert = require('assert')
 var RecordCollector = require('../record_collector.js')
+var Record = require('../record.js')
 
 describe('Record Collector tests', function(){
 
-  var recordCollector = new RecordCollector(30)
+  var recordCollector
+
+  beforeEach(function(){
+    recordCollector = new RecordCollector(3000)
+  })
 
   it('starts with 30 pounds cash', function(){
-    assert.strictEqual(30, recordCollector.cash)
+    assert.strictEqual(3000, recordCollector.cash)
+  })
+
+  it('can buy a record & descrease cash', function(){
+    var record = new Record('Beastie Boys', 'Hello Nasty', 'Hip Hop', 1000)
+    recordCollector.buy(record)
+    assert.strictEqual(2000, recordCollector.cash)
   })
 
 })
